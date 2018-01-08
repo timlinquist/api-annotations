30655ee5b1afde45ef7957d29855f7ba5cc7da64
Pablo Kraan <pablokraan@users.noreply.github.com>
2018-01-08 13:43:10 -0300

    MULE-14373: Define annotations to exclude java elements from the API (#1)

---
+++ src/main/java/org/mule/api/annotation/NoExtend.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.api.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that a class must not be extended by clients.
+ * <p/>
+ * As a class annotated with {@link NoExtend} must not be extended, the protected parts of this class definition will not be
+ * considered part of the API, so they can change without considering those changes as backwards incompatible.
+ * <p/>
+ * WARNING: Ignoring the presence of this annotation can cause compile or runtime problems when a new version is available
+ *
+ * @since 1.0
+ */
+@Documented
+@Target(value = {TYPE})
+public @interface NoExtend {
+
+}
---
+++ src/main/java/org/mule/api/annotation/NoImplement.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.api.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that an interface must not be implemented by clients.
+ *
+ * <p/>
+ * As an interface annotated with {@link NoImplement} must not be implemented, this means that the interface can include new
+ * non default methods without breaking any implementation, so they can change without considering those changes as backwards
+ * incompatible.
+ * <p/>
+ * WARNING: Ignoring the presence of this annotation can cause compile or runtime problems when a new version is available
+ *
+ * @since 1.0
+ */
+@Documented
+@Target(value = {TYPE})
+public @interface NoImplement {
+
+}
