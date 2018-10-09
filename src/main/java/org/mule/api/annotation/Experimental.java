7305f0698a5e53445dfbc652e979c810332dc60e
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2018-10-09 14:06:42 -0300

    EE-6323: Introduce WebSockets connector
    (cherry picked from commit 3f82b8f08502396e6e05d48da9dc7c1b61f0c9f8)

---
+++ src/main/java/org/mule/api/annotation/Experimental.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.api.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Target;
+
+/**
+ * Marks the annotated member as experimental. Experimental features are not to be considered production ready.
+ * MuleSoft does not offer support on them nor guarantees backwards compatibility on them.
+ *
+ * You can try them if you want, but keep in mind that the final version to be GA might not be backwards compatible or even
+ * removed completely.
+ *
+ * @since 1.0.2
+ */
+@Documented
+@Target(value = {TYPE, METHOD})
+public @interface Experimental {
+
+}
