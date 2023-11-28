3b03f9d7d332f4f6490552875cd59ead3bd57f24
Rodrigo Merino <elrodro83@users.noreply.github.com>
2023-11-28 18:43:09 -0300

    W-13829806: Generate mule-module.properties from module-info (#68)
    * Support optionalExportedPackage

---
+++ src/main/java/org/mule/api/annotation/jpms/OptionalPackages.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.api.annotation.jpms;
+
+import static java.lang.annotation.ElementType.MODULE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Defines the optional API of a module that is part of the Mule Runtime Container.
+ * 
+ * @since 1.6
+ */
+@Documented
+@Retention(RUNTIME)
+@Target(MODULE)
+public @interface OptionalPackages {
+
+  /**
+   * Java packages optionally exported by this module.
+   */
+  String[] value();
+
+}
